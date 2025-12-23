f1baac020eedd718aeadbe434f9480063ec02baf
KevinETT <kevintorres9090@gmail.com>
2025-12-23 10:04:19 +0100

    Añadiendo archivos a github

---
+++ public/js/servidores.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const URL = 'https://694a4f4a1282f890d2d84377.mockapi.io/servidores/servidor';
+
+let formulario, tabla;
+
+globalThis.addEventListener('DOMContentLoaded', async () => {
+    formulario = document.forms[0];
+   
+    mostrarTabla();
+});
+
+function anyadir() {
+    
+    formulario.reset();
+    mostrarFormulario();
+
+}
+
+async function guardar() {
+    const cpuPrecio = parseInt(formulario.cpu.value.split('- ')[1] || 0);
+    const ramPrecio = parseInt(formulario.ram.value.split('- ')[1] || 0);
+    const almacenamientoPrecio = parseInt(formulario.Almacenamiento.value.split('- ')[1] || 0);
+    const presupuestoTotal = cpuPrecio + ramPrecio + almacenamientoPrecio;
+
+    formulario.precio.value = presupuestoTotal;
+
+    if (presupuestoTotal > 700) {
+        alert('El presupuesto total no debe superar los 700 euros.');
+        return;
+    }
+
+    const servidor = {
+        nombre: formulario.nombre.value,
+        cpu: formulario.cpu.value,
+        ram: formulario.ram.value,
+        almacenamiento: formulario.Almacenamiento.value,
+        precio: presupuestoTotal
+    };
+
+    const id = formulario.id.value;
+
+    let url, metodo;
+
+     url = URL;
+     metodo = 'POST';
+
+    try {
+        const response = await fetch(url, {
+            method: metodo,
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(servidor),
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+        formulario.reset();
+        mostrarTabla();
+    } catch (error) {
+        console.error(error);
+        alert('Error al guardar el producto');
+    }
+}
+
+async function mostrarTabla() {
+    await recargarTabla();
+
+}
+
+async function recargarTabla() {
+    const respuesta = await fetch(URL);
+    const servidores = await respuesta.json();
+    console.log(servidores);
+    const container = document.getElementById('cards-container');
+    container.innerHTML = '';
+    
+
+    for (const servidor of servidores) {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+        <h3>${servidor.nombre}</h3>
+        <p><strong>CPU:</strong> ${servidor.cpu}</p>
+        <p><strong>RAM:</strong> ${servidor.ram}</p>
+         <p><strong>Almacenamiento:</strong> ${servidor.almacenamiento}</p>
+         <p><strong>Precio Total:</strong> ${servidor.precio}</p>
+    `;
+        container.appendChild(card);
+    }
+}
+
+
